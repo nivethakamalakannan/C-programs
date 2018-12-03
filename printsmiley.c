80d9c50eed534bb47a3cee2a220e8f7c5aad2fc3
nivethakamalakannan <40655463+nivethakamalakannan@users.noreply.github.com>
2018-12-03 16:57:28 +0530

    Add files via upload

---
+++ printsmiley.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+	int sml=1, i, limit;
+	printf("How many smiley face you want to print ? ");
+	scanf("%d",&limit);
+	for(i=0; i<limit; i++)
+	{
+		printf("%c ",sml);
+	}
+	getch();
+}
